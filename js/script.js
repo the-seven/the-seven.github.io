2fd4e1c2e115f350856e39458360595a42284c94
elancer-dev <37080809+elancer-dev@users.noreply.github.com>
2018-03-06 17:37:55 +0300

    c

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    
+    var t1 = atob('KzcgKDkxOCkgNDc3LTA2LTgw');
+    var t2 = atob('KzcgKDk3OCkgMDk3LTQzLTc5');
+    var t3 = atob('KzcgKDk3OCkgNzEwLTQ0LTM5');
+    var m1 = atob('dGhlLXNldmVuQGxpc3QucnU=');
+
+    $('.t1').text(t1);
+    $('.t2').text(t2);
+    $('.t3').text(t3);
+
+    $('.m1').attr({'href': 'mailto:' + m1, 'title': m1}).text(m1);
+
     $('.input-submit').click(function(){
         $('.submit-form .form').slideUp(400,function(){
             $('.submit-done').slideDown(400);
